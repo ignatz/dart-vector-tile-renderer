3be3617316ba60112fdecf106271cb6c658cf827
Sebastian <sjeltsch@gmail.com>
2024-02-09 11:09:23 +0100

    Make different fill render modes configurable to experiment, e.g. vertex batching, path batching, ...

--- lib/src/features/fill_renderer.dart
+++ lib/src/features/fill_renderer.dart
@@ -7,6 +7,7 @@ import '../themes/style.dart';
 import 'extensions.dart';
 import 'feature_renderer.dart';
 
+final batchedPath = ui.Path();
 final List<double> batchedTriangles = [];
 ui.Paint? prevPaint;
 int batchSize = 0;
@@ -42,18 +43,27 @@ class FillRenderer extends FeatureRenderer {
     final outlinePaint =
         style.outlinePaint?.evaluate(evaluationContext)?.paint();
 
+    // Unfortunately, we cannot batch outlines + fill because it would change the ordering.
     if (outlinePaint != null) {
-      final polygons = feature.paths;
-      for (final polygon in polygons) {
-        if (!context.optimizations.skipInBoundsChecks &&
-            !context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
-          continue;
+      if (fillPaint == null) {
+        final path = ui.Path();
+        for (final polygon in feature.paths) {
+          if (!context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
+            continue;
+          }
+          path.addPath(polygon.path, const ui.Offset(0, 0));
         }
-        if (fillPaint != null) {
-          context.canvas.drawPath(polygon.path, fillPaint);
-        }
+        context.canvas.drawPath(path, outlinePaint);
+      } else {
+        print('WARN expensive outline paint');
+        for (final polygon in feature.paths) {
+          if (!context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
+            continue;
+          }
 
-        context.canvas.drawPath(polygon.path, outlinePaint);
+          context.canvas.drawPath(polygon.path, fillPaint);
+          context.canvas.drawPath(polygon.path, outlinePaint);
+        }
       }
       return;
     }
@@ -62,40 +72,95 @@ class FillRenderer extends FeatureRenderer {
       return;
     }
 
-    const drawVertices = true;
-    if (drawVertices) {
-      // Flush previous.
-      if (prevPaint != null && prevPaint != fillPaint) {
-        context.canvas.drawVertices(
-            toVerticesDouble(batchedTriangles), ui.BlendMode.src, prevPaint!);
+    // Different draw modes
+    // 0: most aggressive: batch vertices across features and drawVertices
+    // 1: batch within the local set of polygons and use drawVertices
+    // 2: batch paths across features but use drawPath
+    // 3: batch within the local feature and use drawPaths.
+    const mode = 2;
+    switch (mode) {
+      case 0:
+        // Flush previous.
+        if (prevPaint != null &&
+            prevPaint != fillPaint &&
+            batchedTriangles.isNotEmpty) {
+          context.canvas.drawVertices(
+              toVerticesDouble(batchedTriangles), ui.BlendMode.src, prevPaint!);
 
-        //print(batchSize);
-        batchSize = 0;
-        batchedTriangles.clear();
-        prevPaint = null;
-      }
-
-      batchSize++;
-      prevPaint = fillPaint;
-      final triangles = batchedTriangles;
-
-      final clip = context.tileSpaceMapper.tileClipInTileUnits;
-      feature.pushTrianglePointsDouble(clip, triangles);
-
-      // final vertices = feature.getVertices(clip);
-      // context.canvas.drawVertices(vertices, ui.BlendMode.src, fillPaint);
-    } else {
-      final batchedPath = ui.Path();
-
-      final polygons = feature.paths;
-      for (final polygon in polygons) {
-        if (!context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
-          continue;
+          //print(batchSize);
+          batchSize = 0;
+          batchedTriangles.clear();
+          prevPaint = null;
         }
-        batchedPath.addPath(polygon.path, const ui.Offset(0, 0));
 
-        context.canvas.drawPath(batchedPath, fillPaint);
-      }
+        batchSize++;
+        prevPaint = fillPaint;
+
+        final clip = context.tileSpaceMapper.tileClipInTileUnits;
+        feature.pushTrianglePointsDouble(clip, batchedTriangles);
+
+        break;
+      case 1:
+        final List<double> triangles = [];
+
+        feature.pushTrianglePointsDouble(null, triangles);
+        context.canvas.drawVertices(
+            toVerticesDouble(triangles), ui.BlendMode.src, fillPaint);
+
+        break;
+      case 2:
+        // Flush previous.
+        if (prevPaint != null && prevPaint != fillPaint && batchSize > 0) {
+          context.canvas.drawPath(batchedPath, prevPaint!);
+
+          batchSize = 0;
+          batchedPath.reset();
+          prevPaint = null;
+        }
+
+        batchSize++;
+        prevPaint = fillPaint;
+
+        for (final polygon in feature.paths) {
+          if (!context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
+            continue;
+          }
+          batchedPath.addPath(polygon.path, const ui.Offset(0, 0));
+        }
+
+        break;
+      case 3:
+        final path = ui.Path();
+
+        for (final polygon in feature.paths) {
+          if (!context.tileSpaceMapper.isPathWithinTileClip(polygon)) {
+            continue;
+          }
+          path.addPath(polygon.path, const ui.Offset(0, 0));
+        }
+
+        context.canvas.drawPath(path, fillPaint);
+
+        break;
+    }
+
+    if (mode == 1 &&
+        forceFlush &&
+        prevPaint != null &&
+        batchedTriangles.isNotEmpty) {
+      context.canvas.drawVertices(
+          toVerticesDouble(batchedTriangles), ui.BlendMode.src, prevPaint!);
+
+      batchedTriangles.clear();
+      prevPaint = null;
+      batchSize = 0;
+    }
+
+    if (mode == 2 && forceFlush && prevPaint != null && batchSize > 0) {
+      context.canvas.drawPath(batchedPath, prevPaint!);
+      batchedPath.reset();
+      prevPaint = null;
+      batchSize = 0;
     }
 
     if (forceFlush && prevPaint != null) {
